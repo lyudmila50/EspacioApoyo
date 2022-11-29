d98df337343d470abb0379806ca57379f52d2a27
lyudmila50 <81586606+lyudmila50@users.noreply.github.com>
2022-11-29 09:55:32 -0300

    login

---
+++ prisma/controllers/sessions-controller.js
@@ -0,0 +1,4 @@
+function  login() {
+
+  console.log("Este es un login ")  
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import express from "express";
+import express, { Router } from "express";
 
-const app = express ();
+const app = express();
 app.use(express.json());
- 
-app.listen(3000,() =>
-console.log('Bienvenido a mi apiiiiiiiiii <3'),
+
+app.listen(3000, () =>
+    console.log('Bienvenido a mi apiiiiiiiiii <3'),
 
 )
+export const App = () => {
+    return (
+        <AuthProvider>
+            <Route exact path="/">
+                <LoginPage />
+            </Route>
+            
+        </AuthProvider>
+    );
+}
